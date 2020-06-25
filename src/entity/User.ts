b040802a1bb838df4ee5ae78aa4c7e1eae75edbd
sternelee <sternelee@gmail.com>
2020-06-26 02:52:06 +0800

    更新

---
+++ src/entity/User.ts
@@ -0,0 +1,38 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+// 用户
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  uid: string;
+
+  @Column({
+    default: null
+  })
+  platform: string
+
+  @Column({
+    default: null,
+  })
+  username: string;
+
+  @Column({
+    default: null,
+  })
+  avatar: string;
+
+  @Column()
+  create_time: Date;
+
+  @Column()
+  last_login: Date;
+
+  @Column({
+    default: null,
+  })
+  email: string;
+}
