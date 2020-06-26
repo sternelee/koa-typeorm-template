6c8e9ebde083baad4fd7be4c634fbef8a3aa32a4
sternelee <sternelee@gmail.com>
2020-06-26 10:35:42 +0800

    更新

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,6 @@
 import HomeController from "./controller/home-controller";
 import OauthController from "./controller/oauth-controller";
 import PostController from "./controller/post-controller";
-import DailyController from "./controller/daily-controller";
 import UserController from "./controller/user-controller";
 
 export default [
@@ -34,31 +33,6 @@ export default [
     path: "/post/find",
     method: "get",
     action: PostController.find,
-  },
-  {
-    path: "/daily/popular",
-    method: "get",
-    action: DailyController.popular,
-  },
-  {
-    path: "/daily/tags",
-    method: "get",
-    action: DailyController.tags,
-  },
-  {
-    path: "/daily/settings",
-    method: "get",
-    action: DailyController.settings,
-  },
-  {
-    path: "/daily/publications",
-    method: "get",
-    action: DailyController.publications,
-  },
-  {
-    path: "/daily/graphql",
-    method: "get",
-    action: DailyController.graphql,
   },
   {
     path: "/user/me",
